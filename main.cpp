249053e0f9b79e47a09b0d6057ff86815b86d003
Pranav T <143324058+cokenotfound@users.noreply.github.com>
2024-11-30 16:19:57 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,18 +14,41 @@ class Vehicle
         int engine_capacity;
         bool public;
         string payment;
+        float toll_fee;
+    public:
+        Vehicle():
+        {}
+
+        virtual toll(){}
+
+        virtual valid(){}
+
 };
 
-
-class Single_Axle
+class Single_Axle: public Vehicle
 {
     /* 
     axle=1;
     weight= 100kg -1000kg
     engine capacity- 100cc to 1000cc;
-
-
     */
+    Single_Axle():{}
+
+    Single_Axle(float weight, int axle, int prev_toll_distance, string fuel, int manufacture_date, string registation,string toll_type,int engine_capacity,bool public,string payment)
+    {
+        this -> weight = weight;
+        this -> axle = axle;
+        this -> prev_toll_distance = prev_toll_distance;
+        this -> fuel = fuel;
+        this -> manufacture_date = manufacture_date;
+        this -> registation = registation; 
+        this -> toll_type = toll_type;
+        this -> engine_capacity = engine_capacity;
+        bool -> public = public;
+        string payment;
+    }
+
+    void print_details()
 };
 
 class Double_Axle
@@ -36,6 +59,23 @@ class Double_Axle
     weight= 1000kg - 5000kg;
     engine capacity- 1000cc to 5000cc
     */
+
+    Double_Axle():{}
+
+    Double_Axle(float weight, int axle, int prev_toll_distance, string fuel, int manufacture_date, string registation,string toll_type,int engine_capacity,bool public,string payment)
+    {
+        this -> weight = weight;
+        this -> axle = axle;
+        this -> prev_toll_distance = prev_toll_distance;
+        this -> fuel = fuel;
+        this -> manufacture_date = manufacture_date;
+        this -> registation = registation; 
+        this -> toll_type = toll_type;
+        this -> engine_capacity = engine_capacity;
+        bool -> public = public;
+        string payment;
+    }
+
 
 };
 
@@ -48,6 +88,21 @@ class Triple_Axle
     engine capacity-  5000cc - 8000cc
     goods_weight - max 10000kg
         */
+    Triple_Axle():{}
+
+    Triple_Axle(float weight, int axle, int prev_toll_distance, string fuel, int manufacture_date, string registation,string toll_type,int engine_capacity,bool public,string payment)
+    {
+        this -> weight = weight;
+        this -> axle = axle;
+        this -> prev_toll_distance = prev_toll_distance;
+        this -> fuel = fuel;
+        this -> manufacture_date = manufacture_date;
+        this -> registation = registation; 
+        this -> toll_type = toll_type;
+        this -> engine_capacity = engine_capacity;
+        bool -> public = public;
+        string payment;
+    }
 };
 
 class Quadruple_Axle
@@ -60,4 +115,19 @@ class Quadruple_Axle
     goods_weight - 10000kg to 20000kg
     
     */
+    Quadruple_Axle():{}
+
+    Quadruple_Axle(float weight, int axle, int prev_toll_distance, string fuel, int manufacture_date, string registation,string toll_type,int engine_capacity,bool public,string payment)
+    {
+        this -> weight = weight;
+        this -> axle = axle;
+        this -> prev_toll_distance = prev_toll_distance;
+        this -> fuel = fuel;
+        this -> manufacture_date = manufacture_date;
+        this -> registation = registation; 
+        this -> toll_type = toll_type;
+        this -> engine_capacity = engine_capacity;
+        bool -> public = public;
+        string payment;
+    }
 };
