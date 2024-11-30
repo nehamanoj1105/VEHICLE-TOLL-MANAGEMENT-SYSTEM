08183941ec444e808ae6c220824e48763b569303
Pranav T <143324058+cokenotfound@users.noreply.github.com>
2024-11-30 15:56:27 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+class Vehicle
+{
+    private:
+        float weight;
+        int axle;
+        int prev_toll_distance;
+        string fuel;
+        int manufacture_date;
+        string registation; //state of registration
+        string toll_type;
+        int engine_capacity;
+        bool public;
+        string payment;
+};
+
