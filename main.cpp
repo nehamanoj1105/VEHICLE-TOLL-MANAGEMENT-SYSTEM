d19d62a36a90b88fa7aff4ec8c0df3f275e17363
Pranav T <143324058+cokenotfound@users.noreply.github.com>
2024-11-30 15:58:31 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,3 +16,48 @@ class Vehicle
         string payment;
 };
 
+
+class Single_Axle
+{
+    /* 
+    axle=1;
+    weight= 100kg -1000kg
+    engine capacity- 100cc to 1000cc;
+
+
+    */
+};
+
+class Double_Axle
+{
+
+    /*
+    axle=2;
+    weight= 1000kg - 5000kg;
+    engine capacity- 1000cc to 5000cc
+    */
+
+};
+
+class Triple_Axle
+{
+    int goods_weight;
+    /*
+    axle =3;
+    weight= 5000kg - 8000kg
+    engine capacity-  5000cc - 8000cc
+    goods_weight - max 10000kg
+        */
+};
+
+class Quadruple_Axle
+{
+    int goods_weight;
+    /*
+    axle=4;
+    weight= 8000kg to 10000kg
+    engine capacity=8000cc+
+    goods_weight - 10000kg to 20000kg
+    
+    */
+};
